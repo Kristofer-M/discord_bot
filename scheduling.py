7e17c3eb1c23d1bb2a440b179939c3299b5597f4
Kristofer <kris_awesome@hotmail.com>
2022-03-06 14:55:47 +0100

    add scheduling.py

---
+++ scheduling.py
@@ -0,0 +1,63 @@
+import datetime
+from dateutil import parser
+from discord.ext import tasks
+import asyncio
+
+import main
+
+scheduled_tasks = {}
+
+
+class ScheduledTask:
+    def __init__(self, name, context, wait_time, schedule_name, user_names):
+        self.name = name
+        self.task = self.alarm_start.start(context, wait_time, schedule_name, user_names)
+        self.context = context
+
+    @tasks.loop(count=1)
+    async def alarm_start(self, context, wait_time, schedule_name, user_names):
+        print(f'The name of the alarm is: {schedule_name}')
+        await asyncio.sleep(wait_time)
+        scheduled_tasks[schedule_name] = self.repeat_weekly.start(context, schedule_name, user_names)
+        self.task = scheduled_tasks[schedule_name]
+
+    @tasks.loop(seconds=2)
+    async def repeat_weekly(self, context, alarm_name, user_names):
+        await main.send_message(context, f'{alarm_name} {user_names}')
+
+    def cancel(self):
+        self.task.cancel()
+
+
+async def stop(context, schedule_name):
+    scheduled_tasks[schedule_name].cancel()
+    await main.send_message(context, f'{schedule_name} stopped.')
+
+
+async def stop_all(context):
+    for alarm_name in scheduled_tasks:
+        scheduled_tasks[alarm_name].cancel()
+    await main.send_message(context, "All alarms stopped.")
+
+
+async def alarm(context, day, time, *args):
+    dt = datetime.datetime
+    user_names = []
+    alarm_name = []
+    args = [arg for tup in args for arg in tup]
+
+    for arg in args:
+        if str(arg).startswith('<@'):
+            user_names.append(arg)
+        else:
+            alarm_name.append(arg)
+    user_names = ' '.join(user_names)
+    alarm_name = ' '.join(alarm_name)
+
+    alarm = f'{day} {time}'
+    await main.send_message(context, "Alarm set for {0}".format(str(alarm)))
+    future_date = parser.parse(alarm)
+    wait_time = future_date - dt.now()
+    wait_time = wait_time.seconds
+    print(f'The name of the alarm is: {alarm_name}')
+    scheduled_tasks[alarm_name] = ScheduledTask(alarm_name, context, wait_time, alarm_name, user_names)
